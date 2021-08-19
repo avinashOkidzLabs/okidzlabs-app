e723df22574d2d8727f7a95ae9f937a9490137d4
avinashOkidzLabs <avinash.kumar@okidzlabs.com>
2021-08-19 12:51:43 +0530

    [SplashScreen Animation]: Added Animation to SplashScreen

--- app/src/main/java/com/android/okidzlabs/SplashScreen.kt
+++ app/src/main/java/com/android/okidzlabs/SplashScreen.kt
@@ -2,16 +2,29 @@ package com.android.okidzlabs
 
 import android.app.ActivityOptions
 import android.content.Intent
+import android.graphics.drawable.AnimationDrawable
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Handler
+import android.view.View
 import android.view.WindowManager
+import android.view.animation.Animation
+import android.view.animation.AnimationUtils
+import android.widget.ImageView
+import androidx.constraintlayout.widget.ConstraintLayout
 
 @Suppress("DEPRECATION")
 class SplashScreen : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash_screen)
+
+        val constraintLayout = findViewById<ConstraintLayout>(R.id.constraintLayout)
+        val animDrawable = constraintLayout.background as AnimationDrawable
+
+        animDrawable.setEnterFadeDuration(400)
+        animDrawable.setExitFadeDuration(1200)
+        animDrawable.start()
 
         window.setFlags(
             WindowManager.LayoutParams.FLAG_FULLSCREEN,
@@ -20,8 +33,9 @@ class SplashScreen : AppCompatActivity() {
 
         Handler().postDelayed({
             val intent = Intent(this, MainActivity::class.java)
-            startActivity(intent, ActivityOptions.makeSceneTransitionAnimation(this).toBundle())
+            startActivity(intent)
             finish()
-        }, 3000)
+        }, 1200)
+
     }
 }
